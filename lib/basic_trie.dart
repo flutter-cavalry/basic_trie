bf55800b9d886bebaec372d17f59cf9c10f53712
Mgen <m@mgenware.com>
2023-01-24 02:18:13 +0800

    Minor tweaks

--- lib/basic_trie.dart
+++ lib/basic_trie.dart
@@ -1,8 +1,3 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library basic_trie;
 
 export 'src/basic_trie_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
